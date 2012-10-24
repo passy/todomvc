822e5acb93d5135d4db08ddc5071339c68506240
Pascal Hartig <phartig@rdrei.net>
2012-10-24 11:16:52 +0200

    Added a simple test case that adds a new todo item

--- tests/casperjs/tests.js
+++ tests/casperjs/tests.js
@@ -1,18 +1,32 @@
 var casper = require('casper').create();
-// TODO: Make URL based on command line argument and host own static server.
+// TODO: Launch a local server from the test runner.
 var URL = 'http://localhost:8000/architecture-examples/angularjs-perf/';
 
 casper.start(URL, function () {
-    this.test.assertTitleMatch(/TodoMVC$/);
+	this.test.assertTitleMatch(/TodoMVC$/);
 
-    this.test.assertEval(function () {
-        return document.querySelectorAll("#todo-list li").length === 0;
-    }, "TODO list is empty at the start");
+	this.test.assertEval(function () {
+		return document.querySelectorAll('#todo-list li').length === 0;
+	}, 'list is empty at the start');
 
-    this.test.assertEquals(this.fetchText("#todo-count strong"), "0",
-                           "TODO count is 0 at the start");
+	this.test.assertEquals(this.fetchText('#todo-count strong'), '0',
+						   'list count is 0 at the start');
+});
+
+casper.then(function () {
+	this.page.sendEvent('keydown', 'Some Task');
+	this.page.sendEvent('keydown', this.page.event.key.Enter);
+
+	this.test.assertEval(function () {
+		return document.querySelectorAll('#todo-list li').length === 1;
+	}, 'list contains one item');
+
+	this.test.assertEquals(this.fetchText('#todo-list li:first-child label'),
+						   'Some Task');
 });
 
 casper.run(function () {
-    this.test.renderResults(true);
+	this.test.renderResults(true);
 });
+
+// vim:noexpandtab:
